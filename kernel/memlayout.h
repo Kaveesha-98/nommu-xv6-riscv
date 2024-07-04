4922f91711a8a05835e4b9cf201f0ddbaed54b6c
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2024-07-04 14:08:42 +0530

    Change clint address

--- kernel/memlayout.h
+++ kernel/memlayout.h
@@ -26,7 +26,7 @@
 #define VIRTIO0_IRQ 1
 
 // core local interruptor (CLINT), which contains the timer.
-#define CLINT 0x2000000L
+#define CLINT 0x10000000L
 #define CLINT_MTIMECMP(hartid) (CLINT + 0x4000 + 8*(hartid))
 #define CLINT_MTIME (CLINT + 0xBFF8) // cycles since boot.
 
